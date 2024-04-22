a27ae5ef06f159ab4a74ec86025e04949b236d57
s4nzok <sanjokbhandari12@gmail.com>
2024-04-22 14:16:27 +0545

    buttons_added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,157 @@
 
+// import "./App.css"
+// import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+// import useClipboard from "react-use-clipboard";
+// import { useState, useRef } from "react";
+// import { BiSolidMicrophone, BiCopy, BiPauseCircle } from 'react-icons/bi'
+// import { GrPowerReset } from 'react-icons/gr'
+
+// const SuccessAnimation = (props) => {
+//     // Add your success animation JSX here
+//     return (
+//         <div className="success-animation">
+//           {
+//             props.status ? (
+//                 <span className="success-span">Success! Mantra matched!</span>
+//             ) : (
+//                 <span className="error-span">Alert! Mantra not matched, Try again !!!! </span>
+//             )
+//             }
+//             <video src="/assets/animation/success.mp4"></video>
+//         </div>
+//     );
+
+
+// };
+
+
+// const App = () => {
+//     const [textToCopy, setTextToCopy] = useState();
+//     const [isCopied, setCopied] = useClipboard(textToCopy, {
+//         successDuration: 1000
+//     });
+//     const [successStaus, setSuccessStatus]= useState(false);
+
+//     const [selectedMantra, setSelectedMantra] = useState('Hare Krishna Hare Krishna Krishna Krishna Hare Hare Hare Ram Hare Ram Ram Ram Hare Hare');
+
+//     const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
+//     const { transcript, browserSupportsSpeechRecognition, resetTranscript } = useSpeechRecognition();
+//     const [isSuccessVisible, setIsSuccessVisible] = useState(false);
+//     const [isUnsuccessVisible, setIsUnsuccessVisible] = useState(false);
+
+//     const handleResetTranscript = () => {
+//         resetTranscript(); // This will reset the transcript value to an empty string
+//     };
+
+//     if (!browserSupportsSpeechRecognition) {
+//         return null
+//     }
+  
+//     function preprocessInput(input) {
+//         // Remove punctuation and convert to lowercase
+//         return input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').toLowerCase();
+//       }
+      
+//       const checkMatchingMantra = (spokenText) => {
+//         // return selectedMantra.includes(spokenText);
+//         console.log(spokenText);
+//         console.log(selectedMantra);
+//         if (preprocessInput(spokenText) === preprocessInput(selectedMantra)) {
+//             console.log("success");
+//             return true;
+//         } else {
+//             console.log("error");
+//             return false;
+//         }
+//     };
+
+
+//     const handleMatch = (spokenText) => {
+//         if (checkMatchingMantra(spokenText)) {
+//             setSuccessStatus(true);
+//             setIsSuccessVisible(true);
+//             //   playSound();
+//             setTimeout(() => {
+//                 setIsSuccessVisible(false);
+//             }, 3000);
+//         } else {
+//             setSuccessStatus(false);
+//             setIsSuccessVisible(true);
+//             setTimeout(() => {
+//                 setIsSuccessVisible(false);
+//             }, 3000);
+//         }
+//     };
+
+
+
+//     return (
+//         <>
+//             <div className="container">
+//                 <h2>Test Your Chanting </h2>
+//                 <br />
+//                 <select className="select" onChange={(e) => setSelectedMantra(e.target.value)}>
+//                     <option>Hare Krishna Hare Krishna Krishna Krishna Hare Hare Hare Ram Hare Ram Ram Ram Hare Hare </option>
+//                     <option>Om Krishnaya Namaha</option>
+//                     <option>Om Krishnaya Namaha</option>
+//                     <option>Om Krishnaya Namaha</option>
+//                     <option>Om Krishnaya Namaha</option>
+//                     <option>Aum Devkinandanaye Vidmahe Vasudevaye Dhi-Mahi Tanno Krishna Prachodayat</option>
+//                     <option> Om Sri Krishnah sharanam mamah </option>
+//                 </select>
+
+//                 <div className="main-content" onClick={() => setTextToCopy(transcript)}>
+//                     {transcript}
+//                 </div>
+
+//                 {isSuccessVisible && <SuccessAnimation status={successStaus} />}
+                
+
+//                 <div className="btn-style">
+
+//                     <button onClick={handleResetTranscript}> <GrPowerReset /> </button>
+//                     <button onClick={setCopied}>
+//                         <BiCopy />
+//                         {isCopied ? 'Copied!' : ''}
+//                     </button>
+//                     <button onClick={startListening}> <BiSolidMicrophone /> </button>
+//                     <button onClick={SpeechRecognition.stopListening}> <BiPauseCircle /></button>
+//                     <button onClick={() => handleMatch(transcript)}> check </button>
+
+//                 </div>
+
+//             </div>
+
+//         </>
+//     );
+// };
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import "./App.css"
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import useClipboard from "react-use-clipboard";
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { BiSolidMicrophone, BiCopy, BiPauseCircle } from 'react-icons/bi'
 import { GrPowerReset } from 'react-icons/gr'
 
 const SuccessAnimation = (props) => {
-    // Add your success animation JSX here
     return (
         <div className="success-animation">
           {
@@ -20,57 +164,40 @@ const SuccessAnimation = (props) => {
             <video src="/assets/animation/success.mp4"></video>
         </div>
     );
-
-
 };
-
 
 const App = () => {
     const [textToCopy, setTextToCopy] = useState();
     const [isCopied, setCopied] = useClipboard(textToCopy, {
         successDuration: 1000
     });
-    const [successStaus, setSuccessStatus]= useState(false);
-
+    const [successStatus, setSuccessStatus]= useState(false);
     const [selectedMantra, setSelectedMantra] = useState('Hare Krishna Hare Krishna Krishna Krishna Hare Hare Hare Ram Hare Ram Ram Ram Hare Hare');
 
     const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
     const { transcript, browserSupportsSpeechRecognition, resetTranscript } = useSpeechRecognition();
     const [isSuccessVisible, setIsSuccessVisible] = useState(false);
-    const [isUnsuccessVisible, setIsUnsuccessVisible] = useState(false);
 
     const handleResetTranscript = () => {
-        resetTranscript(); // This will reset the transcript value to an empty string
+        resetTranscript();
     };
-
-    if (!browserSupportsSpeechRecognition) {
-        return null
-    }
   
     function preprocessInput(input) {
-        // Remove punctuation and convert to lowercase
         return input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').toLowerCase();
-      }
+    }
       
-      const checkMatchingMantra = (spokenText) => {
-        // return selectedMantra.includes(spokenText);
-        console.log(spokenText);
-        console.log(selectedMantra);
+    const checkMatchingMantra = (spokenText) => {
         if (preprocessInput(spokenText) === preprocessInput(selectedMantra)) {
-            console.log("success");
             return true;
         } else {
-            console.log("error");
             return false;
         }
     };
-
 
     const handleMatch = (spokenText) => {
         if (checkMatchingMantra(spokenText)) {
             setSuccessStatus(true);
             setIsSuccessVisible(true);
-            //   playSound();
             setTimeout(() => {
                 setIsSuccessVisible(false);
             }, 3000);
@@ -83,7 +210,9 @@ const App = () => {
         }
     };
 
-
+    if (!browserSupportsSpeechRecognition) {
+        return null
+    }
 
     return (
         <>
@@ -93,22 +222,18 @@ const App = () => {
                 <select className="select" onChange={(e) => setSelectedMantra(e.target.value)}>
                     <option>Hare Krishna Hare Krishna Krishna Krishna Hare Hare Hare Ram Hare Ram Ram Ram Hare Hare </option>
                     <option>Om Krishnaya Namaha</option>
-                    <option>Om Krishnaya Namaha</option>
-                    <option>Om Krishnaya Namaha</option>
-                    <option>Om Krishnaya Namaha</option
                     <option>Aum Devkinandanaye Vidmahe Vasudevaye Dhi-Mahi Tanno Krishna Prachodayat</option>
-                    <option> Om Sri Krishnah sharanam mamah </option>
+                    <option>Om Sri Krishnah sharanam mamah</option>
                 </select>
 
                 <div className="main-content" onClick={() => setTextToCopy(transcript)}>
                     {transcript}
                 </div>
 
-                {isSuccessVisible && <SuccessAnimation status={successStaus} />}
+                {isSuccessVisible && <SuccessAnimation status={successStatus} />}
                 
 
                 <div className="btn-style">
-
                     <button onClick={handleResetTranscript}> <GrPowerReset /> </button>
                     <button onClick={setCopied}>
                         <BiCopy />
@@ -117,11 +242,11 @@ const App = () => {
                     <button onClick={startListening}> <BiSolidMicrophone /> </button>
                     <button onClick={SpeechRecognition.stopListening}> <BiPauseCircle /></button>
                     <button onClick={() => handleMatch(transcript)}> check </button>
-
+                    <button onClick={() => setSelectedMantra('Om Krishnaya Namaha')}>Om Krishnaya Namaha</button>
+                    <button onClick={() => setSelectedMantra('Aum Devkinandanaye Vidmahe Vasudevaye Dhi-Mahi Tanno Krishna Prachodayat')}>Aum Devkinandanaye Vidmahe Vasudevaye Dhi-Mahi Tanno Krishna Prachodayat</button>
+                    <button onClick={() => setSelectedMantra('Om Sri Krishnah sharanam mamah')}>Om Sri Krishnah sharanam mamah</button>
                 </div>
-
             </div>
-
         </>
     );
 };
